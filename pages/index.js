95c3b4ef6453478801528690774cdbd46bd9add7
alyarusydam@gmail.com <alyarusydam@gmail.com>
2023-03-03 10:52:43 +0700

    ganti hero dikit

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,26 @@
 "use client";
-import About from '@/components/About';
-import Food from '@/components/Food'
-import Headline from '@/components/Headline'
-import Hero from '@/components/Hero'
-import withNavbar from '@/components/layout/withNavbar';
+import About from "@/components/About";
+import Food from "@/components/Food";
+import Headline from "@/components/Headline";
+import Hero from "@/components/Hero";
+import withNavbar from "@/components/layout/withNavbar";
 // import styles from '../styles/globals.css'
 
 function Home() {
   return (
     <>
-    <Hero />
-    <Headline />
-    {/* <section id='about'> */}
-      <About />
-    {/* </section> */}
-    <section id='favorit'>
-      <Food />
-    </section>
-    
+      <div className="overflow-x-hidden">
+        <Hero />
+        <Headline />
+        {/* <section id='about'> */}
+        <About />
+        {/* </section> */}
+        <section id="favorit">
+          <Food />
+        </section>
+      </div>
     </>
-  )
+  );
 }
 
-export default withNavbar(Home)
+export default withNavbar(Home);
