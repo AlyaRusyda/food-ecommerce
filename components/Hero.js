b4d7f9eeca27b80c31903be49c69d8ed965ce0c2
alyarusydam@gmail.com <alyarusydam@gmail.com>
2023-03-03 09:33:29 +0700

    ganti hero dikit

--- components/Hero.js
+++ components/Hero.js
@@ -6,8 +6,8 @@ const Hero = () => {
         <div className='max-h-[500px] relative mt-16'>
             {/* Overlay */}
             <div className='absolute w-full h-full text-gray-200 max-h-[500px] bg-black/40 flex flex-col justify-center'>
-                <h1 className='px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold'><span className='text-orange-500'>Pesanannya</span> Gak</h1>
-                <h1 className='px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold'>Dateng <span className='text-orange-500'>Dateng</span></h1>
+                <h1 className='px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold'>Pesanannya<span className='text-orange-500'> Gak</span></h1>
+                <h1 className='px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold'><span className='text-orange-500'>Dateng </span>Dateng</h1>
             </div>
             <img className='w-full max-h-[500px] object-cover' src="https://images.unsplash.com/photo-1464226184884-fa280b87c399?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80" alt="/" />
         </div>
